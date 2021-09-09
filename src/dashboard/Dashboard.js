69206526aec3359735681b4e9acc75bccce38cb9
Blizzcane <blizzcane@gmail.com>
2021-09-08 21:38:25 -0500

    first commit

---
+++ src/dashboard/Dashboard.js
@@ -0,0 +1,120 @@
+import React from "react";
+import ErrorAlert from "../layout/ErrorAlert";
+import { useHistory } from "react-router-dom";
+import { previous, today, next } from "../utils/date-time";
+import ReservationRow from "./ReservationRow";
+import TableRow from "./TableRow";
+
+/**
+ * Defines the dashboard page.
+ * @param date
+ *  the date for which the user wants to view reservations.
+ * @returns {JSX.Element}
+ */
+
+function Dashboard({
+  date,
+  reservations,
+  reservationsError,
+  tables,
+  tablesError,
+  loadDashboard,
+}) {
+  const history = useHistory();
+
+  const reservationsJSX = () => {
+    return reservations.map((reservation) => (
+      <ReservationRow
+        key={reservation.reservation_id}
+        reservation={reservation}
+        loadDashboard={loadDashboard}
+      />
+    ));
+  };
+  const tablesJSX = () => {
+    return tables.map((table) => (
+      <TableRow
+        key={table.table_id}
+        loadDashboard={loadDashboard}
+        table={table}
+      />
+    ));
+  };
+  //change the date to US friendly
+  const formatDate = (date) => {
+    let splitDate = date.split("-");
+    console.log(splitDate);
+    return `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`;
+  };
+
+  return (
+    <main>
+      <h1>Dashboard</h1><div className="btn-group m-1" role="group">
+        <button
+          type="button"
+          className="btn btn-outline-primary btn-sm"
+          onClick={() => history.push(`/dashboard?date=${previous(date)}`)}
+        >
+          Prev.
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-primary btn-sm"
+          onClick={() => history.push(`/dashboard?date=${today()}`)}
+        >
+          Today
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-primary btn-sm"
+          onClick={() => history.push(`/dashboard?date=${next(date)}`)}
+        >
+          Next
+        </button>
+      </div>
+      <div className="d-md-flex mb-3">
+        <h4 className="mb-0">Reservations for {formatDate(date)}</h4>
+      </div>
+      <ErrorAlert error={reservationsError} />
+      <table className="table table-hover ">
+        <thead className="shadow-sm">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Mobile Number</th>
+            <th scope="col">Time</th>
+            <th scope="col">People</th>
+            <th scope="col">Status</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Cancel</th>
+            <th scope="col">Seat</th>
+          </tr>
+        </thead>
+        <tbody>{reservationsJSX()}</tbody>
+      </table>
+
+      <div className="d-md-flex mb-3">
+        <h4 className="mb-0">Tables</h4>
+      </div>
+      <ErrorAlert error={tablesError} />
+
+      <table className="table">
+        <thead className="shadow-sm">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Table Name</th>
+            <th scope="col">Capacity</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+
+        <tbody>{tablesJSX()}</tbody>
+      </table>
+
+      
+    </main>
+  );
+}
+
+export default Dashboard;
